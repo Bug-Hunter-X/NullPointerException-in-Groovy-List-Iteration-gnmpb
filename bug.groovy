9c1af8cb06934107e58a6aaae654e4fec9e10040
BugHunter-X <askzenai@gmail.com>
2025-01-06 22:10:04 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,13 @@
+```groovy
+def myMethod(List<Integer> numbers) {
+    numbers.each { number ->
+        if (number % 2 == 0) {
+            println "Even: $number"
+        } else {
+            println "Odd: $number"
+        }
+    }
+}
+
+myMethod(null)
+```
